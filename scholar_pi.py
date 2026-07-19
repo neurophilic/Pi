f156d7d18a0a42a9675013cb723d8dae8f19ab8d
neurophilic <167536367+neurophilic@users.noreply.github.com>
2026-07-19 20:22:07 +0200

    Increase gravitational constant in physics options

--- scholar_pi.py
+++ scholar_pi.py
@@ -295,7 +295,7 @@ def generate_interactive_bubble_chart(scope, user_id):
     {
       "physics": {
         "forceAtlas2Based": {
-          "gravitationalConstant": 0.001,
+          "gravitationalConstant": 0.01,
           "centralGravity": 0,
           "springLength": 150,
           "avoidOverlap": 0.5
